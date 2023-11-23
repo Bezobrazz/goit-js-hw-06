b002726b3af7dd9b8d6b09ffbe6e07a6d0246682
Viacheslav Kostenko <kostenko.uvd@gmail.com>
2023-11-23 20:28:34 +0200

    Update task-02.js

--- src/js/task-02.js
+++ src/js/task-02.js
@@ -1,8 +1,26 @@
 const ingredients = [
-  'Potatoes',
-  'Mushrooms',
-  'Garlic',
-  'Tomatos',
-  'Herbs',
-  'Condiments',
+  "Potatoes",
+  "Mushrooms",
+  "Garlic",
+  "Tomatos",
+  "Herbs",
+  "Condiments",
 ];
+const ulElement = document.getElementById("ingredients");
+
+const liElements = ingredients.map((ingredient) => {
+  const liElement = document.createElement("li");
+  liElement.textContent = ingredient;
+  liElement.classList.add("item");
+  return liElement;
+});
+
+ulElement.append(...liElements);
+
+// Цей варіант вважаю більш доцільним та швидшим
+// const container = document.querySelector("#ingredients");
+// const newElement = ingredients
+//   .map((el) => `<li class="item">${el}</li>`)
+//   .join("");
+
+// container.insertAdjacentHTML("afterbegin", newElement);
