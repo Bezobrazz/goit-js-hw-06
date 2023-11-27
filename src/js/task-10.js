01cb9052b87bb903dc9dd70a82e38e0ad1e6a9aa
Viacheslav Kostenko <kostenko.uvd@gmail.com>
2023-11-27 17:03:21 +0200

    Fixes

--- src/js/task-10.js
+++ src/js/task-10.js
@@ -3,3 +3,19 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+// const inputElem = document.querySelector("#controls > input");
+// const createButton = document.querySelector(`[data-create]`);
+// const destroyButton = document.querySelector(`[data-destroy]`);
+// const boxesElem = document.querySelector("#boxes");
+
+// function createBoxes(amount) {
+//   inputElem.addEventListener("change", (event) => {
+//     const numberOFBoxes = event.target.value;
+//     createButton.addEventListener("click", () => {
+//       const result = +numberOFBoxes;
+//       console.log(result);
+//     });
+//   });
+// }
+// createBoxes();
